8ca63590a034efb18c0bb214c386c72c541013d7
Shcherbyna Oleksandr <Scherbinaalexandr75@gmail.com>
2025-02-02 16:32:15 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -7,6 +7,11 @@
 //  функція повинна повернути копію масиву з довжиною maxLength елементів.
 // В іншому випадку функція повинна повернути весь новий масив.
 
+function makeArray(firstArray, secondArray, maxLength) {
+  const newArray = firstArray.concat(secondArray);
+  return newArray.slice(0, maxLength);
+}
+
 console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
 console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
 console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
